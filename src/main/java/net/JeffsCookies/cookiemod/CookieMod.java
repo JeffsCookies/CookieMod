da3636594fd6c7f7e07e2aa296ffe5376cd7c263
Hamed Omayrat <hamed.omayrat002@stud.fh-dortmund.de>
2023-01-29 14:02:53 +0100

    Initial commit

---
+++ src/main/java/net/JeffsCookies/cookiemod/CookieMod.java
@@ -0,0 +1,54 @@
+package net.JeffsCookies.cookiemod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CookieMod.MODID)
+public class CookieMod
+{
+    public static final String MODID = "cookiemod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public CookieMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+        //if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS)
+        //    event.accept(EXAMPLE_BLOCK_ITEM);
+    }
+
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+        }
+    }
+}
